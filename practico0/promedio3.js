569833309ca30bec546f4ff112cddf8dff9eb05b
Ezequiel Zalazar <ezebj39@gmail.com>
2023-05-27 12:03:18 -0300

    resuelve ejercicio 4, punto c

--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -1,5 +1,6 @@
-let num1 = -2;
-let num2 = -3;
-let num3 = -8;
+let num1 = -1.2;
+let num2 = 5.3;
+let num3 = -3.3;
 let promedio = (num1 + num2 + num3) / 3;
 console.log (promedio);
+// resuelve ejercicio 4, punto c
