ac623f1a589e33741678744738e956a3bdce6a11
Ezequiel Zalazar <ezebj39@gmail.com>
2023-06-27 20:21:28 -0300

    resultado ejercicio N°1

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,11 @@
+let i = 10
+while(i >= 0){
+    if(i === 10){
+        console.log(i, "Cuenta regresiva");
+    } else if(i === 0){
+        console.log(i, "lanzamiento!");
+    } else{
+        console.log("%i", i);
+    }
+    i--;
+}
