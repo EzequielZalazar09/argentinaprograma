79ee397c70a1591a9da1b909ec7a8fd5141f668b
Ezequiel Zalazar <ezebj39@gmail.com>
2023-06-27 20:28:09 -0300

    resultado ejercicio extra

--- practico3/listaDeInvitados.js
+++ practico3/listaDeInvitados.js
@@ -10,12 +10,15 @@ for (let i = 0; i < personas.length; i++) {
   }
 }
 
-console.log("La lista de invitados admitidos es:");
+admitidos.sort();
+rechazados.sort();
+
+console.log("La lista ordenada de invitados admitidos es:");
 for (let i = 0; i < admitidos.length; i++) {
   console.log(admitidos[i]);
 }
 
-console.log("La lista de invitados rechazados es:");
+console.log("La lista ordenada de invitados rechazados es:");
 for (let i = 0; i < rechazados.length; i++) {
   console.log(rechazados[i]);
 }
